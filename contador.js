6c90d7ce13ea6146a827b2116ec1c00e19b39664
Gael Alejandro Cordova Diaz <gaelalejandrocordovadiaz@gmail.com>
2024-10-03 19:51:58 -0600

    feat: agregar la función de displayCounter

---
+++ contador.js
@@ -0,0 +1,8 @@
+const readline = require('readline');
+
+let count = 0;
+
+function displayCounter() {
+    console.clear(); // Limpiar la consola para mostrar el nuevo valor
+    console.log(`Contador: ${count}`);
+}
